2e1165949898b4bc0ed68eed9b2ea77c233b74df
kevinsims1 <kevinsims1@outlook.com>
2020-03-09 23:08:01 -0700

    init

--- index.js
+++ index.js
@@ -5,56 +5,56 @@ const numbers = readFileSync('./tesco-title-numbers.csv', { encoding: 'utf8' }).
 
 const START = 'https://eservices.landregistry.gov.uk/wps/portal/Property_Search';
 const getAddress = async (id) => {
-    console.log(`Now checking ${id}`);
-    const nightmare = new Nightmare({ show: true });
-    // Go to initial start page, navigate to Detail search
-    try {
-        await nightmare
-            .goto(START)
-            .wait('.bodylinkcopy:first-child')
-            .click('.bodylinkcopy:first-child');
-    } catch (e) {
-        console.error(e);
-    }
+  console.log(`Now checking ${id}`);
+  const nightmare = new Nightmare({ show: true });
+  // Go to initial start page, navigate to Detail search
+  try {
+    await nightmare
+      .goto(START)
+      .wait('.bodylinkcopy:first-child')
+      .click('.bodylinkcopy:first-child');
+  } catch (e) {
+    console.error(e);
+  }
 
-    // Type the title number into the appropriate box; click submit
-    try {
-        await nightmare
-            .wait('input[name="titleNo"]')
-            .type('input[name="titleNo"]', id)
-            .click('input[value="Search »"]');
-    } catch (e) {
-        console.error(e);
-    }
+  // Type the title number into the appropriate box; click submit
+  try {
+    await nightmare
+      .wait('input[name="titleNo"]')
+      .type('input[name="titleNo"]', id)
+      .click('input[value="Search »"]');
+  } catch (e) {
+    console.error(e);
+  }
 
-    //extract data from the results page
-    try {
-        const result = await nightmare
-          .wait('.w80p')
-          .evaluate(() => {
-            return [...document.querySelectorAll('.w80p')]
-              .map(el => el.innerText);
-          })
-          .end();  return { id, address: result[0], lease: result[1] };
-      } catch(e) {
-        console.error(e);
-        return undefined;
-      }
-      
+  //extract data from the results page
+  try {
+    const result = await nightmare
+      .wait('.w80p')
+      .evaluate(() => {
+        return [...document.querySelectorAll('.w80p')]
+          .map(el => el.innerText);
+      })
+      .end(); return { id, address: result[0], lease: result[1] };
+  } catch (e) {
+    console.error(e);
+    return undefined;
+  }
+
 };
 
-// getAddress(numbers[0])
-//   .then(a => console.dir(a))
-//   .catch(e => console.error(e));
+const series = numbers.reduce(async (queue, number) => {
+  const dataArray = await queue;
+  dataArray.push(await getAddress(number));
+  return dataArray;
+}, Promise.resolve([]));
 
-  const series = numbers.reduce(async (queue, number) => {
-    const dataArray = await queue;
-    dataArray.push(await getAddress(number));
-    return dataArray;
-  }, Promise.resolve([]));
-  
-  series.then(data => {
-    const csvData = csvFormat(data.filter(i => i));
-    writeFileSync('./output.csv', csvData, { encoding: 'utf8' });
-  })
+series.then(data => {
+  const csvData = csvFormat(data.filter(i => i));
+  writeFileSync('./output.csv', csvData, { encoding: 'utf8' });
+})
+  .catch(e => console.error(e));
+
+getAddress(numbers[0])
+  .then(a => console.dir(a))
   .catch(e => console.error(e));
